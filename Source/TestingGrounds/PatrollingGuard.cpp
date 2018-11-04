3cdf9d6a2c0efb271cd6080cc503431ed29a4eec
DESKTOP-47R46TC\EvMachine <murad1985@gmail.com>
2018-11-04 01:02:20 -0500

    BP to CPP

---
+++ Source/TestingGrounds/PatrollingGuard.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PatrollingGuard.h"
+
+
+//void APatrollingGuard::GetPatrolPoints(AActor * ControlledPawn)
+//{
+//	auto MyPawn = Cast<ATP_ThirdPersonCharacter>(ControlledPawn);
+//	PatrolPointsCPP = MyPawn->get
+//
+//}

e2eaa5fa210c85a002c6bcceae947f8c83a907c2
DESKTOP-47R46TC\EvMachine <murad1985@gmail.com>
2018-11-11 03:38:53 -0600

    Gun Class

--- Source/TestingGrounds/Weapons/Gun.h
+++ Source/TestingGrounds/Weapons/Gun.h
@@ -45,10 +45,14 @@ public:
 
 	/** AnimMontage to play each time we fire */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-	class UAnimMontage* FireAnimation;
+	class UAnimMontage* FireAnimation1P;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-	class UAnimInstance* AnimInstance;
+	class UAnimMontage* FireAnimation3P;
+
+	class UAnimInstance* AnimInstance1P;
+
+	class UAnimInstance* AnimInstance3P;
 
 	/** Fires a projectile. */
 	UFUNCTION(BlueprintCallable, Category = Input)
